19b5a84f44bb74166766085fc2877abfd6d63ba9
Yahya Abdulselam <zrosaiko@gmail.com>
2024-03-02 21:41:27 +0300

    mobile main page

--- main1.js
+++ main1.js
@@ -1,14 +1,31 @@
 let prvPos = window.scrollY;
+let original = window.scrollY;
+
 window.addEventListener("scroll", () => {
   const nav = document.querySelector(".navv");
   let afterPos = window.scrollY;
-  if (afterPos > prvPos) {
-    nav.classList.remove("oScroll");
-    nav.classList.add('hide');
 
+  if (afterPos !== 0) {
+    if (afterPos > prvPos) {
+    
+      nav.classList.add("hide");
+    } else {
+      nav.classList.add("oScroll");
+      nav.classList.remove("hide");
+    }
+    prvPos = afterPos;
   } else {
-    nav.classList.add("oScroll");
-    nav.classList.remove('hide');
+    nav.classList.remove("oScroll");
   }
-  prvPos = afterPos;
 });
+
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((e) => {
+//     if (e.isIntersecting) {
+//       e.target.classList.add("show");
+//     }
+//   });
+// });
+// const hiddens = document.querySelectorAllL(".hidden");
+// hiddens.forEach((e) => {
+//   observer.observe(e)});
