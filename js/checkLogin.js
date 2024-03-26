b4a00b47142ecbd59925aad872da269b8233609b
ma2009684 <ma2009684@qu.edu.qa>
2024-03-26 15:35:14 +0300

    logincheck and linked upload product
    moved the login check to a separate file called logincheck.js and added it to main only, and linked the upload product button to take the user to the page.

---
+++ js/checkLogin.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#cart-button").addEventListener("click", () => {
+        if (localStorage.getItem("loggeduser") != null) {
+            window.location.href = "/pages/checkout/checkout.html"
+        } else {
+            window.location.href = "./login.html"
+        }
+        })
+        
+        document.querySelector("#account-button").addEventListener("click", () => {
+        if (localStorage.getItem("loggeduser") != null) {
+            window.location.href = "/pages/checkout/checkout.html"
+        } else {
+            window.location.href = "./login.html"
+        }
+        })
+    
+});
