56ea67687cb46aaf7ffb0eb8de12370e2ba7cfa6
yousef-ym2003576 <yousefbashiiir@gmail.com>
2024-03-28 22:57:33 +0300

    updates

--- js/itemSeller.js
+++ js/itemSeller.js
@@ -89,7 +89,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const quantityLeftLi = document.createElement("li");
     quantityLeftLi.classList.add("quantity-left");
 
-    closeimage.appendChild(closeImgDark);
+    closeimage.appendChild(closeimgDark);
     closeimage.appendChild(closeImg);
     const quantityPurchasedLi = document.createElement("li");
     quantityPurchasedLi.classList.add("quantity-purchased");
@@ -146,3 +146,34 @@ document.addEventListener("DOMContentLoaded", () => {
   renderProductsSale();
   renderProductsHistory();
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  let prvPos = window.scrollY;
+
+  window.addEventListener("scroll", () => {
+    const nav = document.querySelector(".navv");
+    let afterPos = window.scrollY;
+
+    if (afterPos !== 0) {
+      if (afterPos > prvPos) {
+        nav.classList.add("hide");
+      } else {
+        nav.classList.add("oScroll");
+        nav.classList.remove("hide");
+      }
+      prvPos = afterPos;
+    } else {
+      nav.classList.remove("oScroll");
+    }
+  });
+});
+
+function handleUpload() {
+  if (!localStorage.getItem("loggedseller")) {
+    localStorage.setItem("uploadDestination", "../upload-product.html");
+    window.location.href = "../sellerlogin.html";
+  } else {
+    window.location.href = "../upload-product.html";
+  }
+}
+
