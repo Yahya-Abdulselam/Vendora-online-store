4abbc538bdc7ed9ffc5ff4ecccb18b875b425fa6
Yahya Abdulselam <zrosaiko@gmail.com>
2024-03-29 13:35:12 +0300

    balance in header

--- category.js
+++ category.js
@@ -1,31 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const clothingButton = document.querySelector("#clothing");
-  const techButton = document.querySelector("#tech");
-  const bookButton = document.querySelector("#book");
-  const furnitureButton = document.querySelector("#furniture");
-  console.log(2);
+  let buyer = JSON.parse(localStorage.getItem("loggeduser") ?? "{}"); //when the user log in we store his data(current seller)
+  const clothingButton = document.querySelectorAll(".clothing");
+  const techButton = document.querySelectorAll(".tech");
+  const bookButton = document.querySelectorAll(".book");
+  const furnitureButton = document.querySelectorAll(".furniture");
+  const balance = document.querySelector("#balance");
+  const balanceDiv = document.querySelector("#balance-div");
 
-  clothingButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    localStorage.setItem("selectedCategory", "clothing");
-    window.location.href = "/listItem.html";
+  if (buyer?.balance) {
+    balanceDiv.computedStyleMap.display = "flex";
+    balance.textContent = buyer.balance;
+    console.log(1);
+  }
+
+  clothingButton.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      localStorage.setItem("selectedCategory", "clothing");
+      window.location.href = "/listItem.html";
+    });
   });
 
-  techButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    localStorage.setItem("selectedCategory", "tech");
-    window.location.href = "/listItem.html";
+  techButton.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      localStorage.setItem("selectedCategory", "tech");
+      window.location.href = "/listItem.html";
+    });
   });
 
-  bookButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    localStorage.setItem("selectedCategory", "book");
-    window.location.href = "/listItem.html";
+  bookButton.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      localStorage.setItem("selectedCategory", "book");
+      window.location.href = "/listItem.html";
+    });
   });
 
-  furnitureButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    localStorage.setItem("selectedCategory", "furniture");
-    window.location.href = "/listItem.html";
+  furnitureButton.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      localStorage.setItem("selectedCategory", "furniture");
+      window.location.href = "/listItem.html";
+    });
   });
 });
