0df4b5ce1e64fc24dbca4d2553991f2e89921c11
ma2009684 <ma2009684@qu.edu.qa>
2024-03-30 14:20:52 +0300

    Fixed redirection bug for checkout

--- js/buyProduct.js
+++ js/buyProduct.js
@@ -19,11 +19,18 @@ document.addEventListener("DOMContentLoaded", () => {
                     product.quantity = quantityText[i].textContent
                     localStorage.setItem("itemInCart", JSON.stringify(product))
                     window.location.href = "../pages/checkout/checkout-address.html"
-                } else {
-                    alert("product doesn't exist")
-                } 
+                } else{
+                    alert("Product doesn't exist!")
+                }
             } else {
-                location.href = "../login.html"
+                const product = JSON.parse(localStorage.getItem("products")).find(
+                    (product) => product.name + product.sellerId === buyButtons[i].value
+                );
+                product.quantity = quantityText[i].textContent
+                localStorage.setItem("itemInCart", JSON.stringify(product))
+                window.location.href = "../pages/checkout/checkout-address.html"
+                localStorage.setItem("destinationAfterLogin", "checkout.html");
+                window.location.href = "login.html";
             } 
         })
     }
