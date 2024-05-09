88191f3056d926f79bfddec556d494a95300f73c
Yahya Abdulselam <zrosaiko@gmail.com>
2024-05-09 23:15:52 +0300

    data

--- phase2/app/layout.jsx
+++ phase2/app/layout.jsx
@@ -1,12 +1,14 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 export const metadata = {
-  title: "Decks",
+  title: "Vendora",
   description: "",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}
+      <SpeedInsights/></body>
     </html>
   );
 }
